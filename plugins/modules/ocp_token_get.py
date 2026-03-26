c263e1e9592f78c4f844295a88b49b1e8b38cd6a
Adel Amgad <60940565+polaamgad88@users.noreply.github.com>
2026-03-26 13:54:24 +0200

    Update ocp_token_get.py

--- plugins/modules/ocp_token_get.py
+++ plugins/modules/ocp_token_get.py
@@ -1,7 +1,59 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 from ansible.module_utils.basic import AnsibleModule
 import requests
+
+DOCUMENTATION = r'''
+---
+module: ocp_token_get
+short_description: Retrieve an OpenShift OAuth token using credentials
+description:
+    - This module authenticates against the OpenShift OAuth server using a username and password.
+    - It returns a temporary session token (Bearer token) that can be used by other modules.
+options:
+    host:
+        description: The OpenShift API URL (e.g., https://api.cluster.example.com:6443).
+        type: str
+        required: true
+    username:
+        description: OpenShift username for authentication.
+        type: str
+        required: true
+    password:
+        description: OpenShift password for authentication.
+        type: str
+        required: true
+        no_log: true
+    verify_ssl:
+        description: Whether to verify the SSL certificate of the OAuth server.
+        type: bool
+        default: false
+author:
+    - Adel Amgad Messiha (aamgad@redhat.com)
+'''
+
+EXAMPLES = r'''
+- name: Get a temporary OCP token
+  polaamgad88.openshift_day2.ocp_token_get:
+    host: "https://api.mycluster.com:6443"
+    username: "admin"
+    password: "password123"
+    verify_ssl: false
+  register: auth_token
+
+- name: Use the token in another task
+  debug:
+    msg: "My token is {{ auth_token.ocp_token }}"
+'''
+
+RETURN = r'''
+ocp_token:
+    description: The retrieved OpenShift authentication token.
+    returned: success
+    type: str
+    sample: "sha256~vX8..."
+'''
 
 def get_token(oauth_url, username, password, verify_ssl):
     token_url = f"{oauth_url}/oauth/authorize?client_id=openshift-challenging-client&response_type=token"
@@ -23,6 +75,7 @@ def run_module():
         verify_ssl=dict(type='bool', default=False)
     )
     module = AnsibleModule(argument_spec=module_args)
+    
     # Convert API URL to OAuth URL
     oauth_url = module.params['host'].replace("api.", "oauth-openshift.apps.").replace(":6443", "")
     
